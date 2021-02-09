a65f2dba235c9c440913b9128eb3bda29057f265
Fcl95 <fclalbuquerque@gmail.com>
2021-02-09 15:57:10 -0300

    função soma dez com purrr

---
+++ R/somadez.R
@@ -0,0 +1,28 @@
+usethis::use_git_config(user.name = "Fcl95",
+                        user.email = "fclalbuquerque@gmail.com")
+
+
+usethis::browse_github_token()
+
+
+usethis::edit_r_environ()
+
+usethis::use_git()
+
+
+usethis::use_github()
+########################
+#funcao soma dez
+
+vect <- 1:5
+
+soma_dez <- function(x) {
+
+  x + 10
+
+}
+
+
+d <- purrr::map_dbl(vect, soma_dez)
+d
+#####################
